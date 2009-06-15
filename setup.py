ab1318bcc38a1d15fee768002c6cfc06625bb576
mvantellingen <none@none>
2009-06-15 21:40:09 +0200

    add basic setup.py script

---
+++ setup.py
@@ -0,0 +1,31 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+    
+version = '0.1'
+
+setup(
+    name="pyjsparser",
+    version=version,
+    description="Python Javascript lexer/parser",
+    long_description="""""",
+    keywords='javascript ecmascript lexer parser ply',
+    license='BSD License',
+    author='Michael van Tellingen',
+    author_email='michaelvantellingen@gmail.com',
+    url='',
+    packages=['pyjsparser'],
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        "ply>=3.2"  
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: JavaScript",
+    ]
+)
